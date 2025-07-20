f83a1983066fed5f629c9342069ea6ef192c2edd
Pratik5868 <pratikpatil5567@gmail.com>
2025-07-20 19:19:28 +0530

    Third Commit with Student class

--- First.java
+++ First.java
@@ -2,5 +2,7 @@ public class First {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("THis is a test Java program to print messages.");
+        Student student = new Student("Alice", 20);
+        System.out.println(student);
     }
 }
