828ac80a6e748ddbc2fa240976470a0b8aad5a17
Pratik5868 <pratikpatil5567@gmail.com>
2025-07-20 19:10:37 +0530

    Second Commit

--- First.java
+++ First.java
@@ -1,5 +1,6 @@
 public class First {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("THis is a test Java program to print messages.");
     }
 }
